3cf1ca1658e67c5be7ecef429fc619ad3df8ed00
yukari4545 <tsurumaki0397@icloud.com>
2025-03-15 22:20:04 +0900

    Update sshCracker.ts

--- src/types/sshCracker.ts
+++ src/types/sshCracker.ts
@@ -1,12 +1,9 @@
-import { NodeSSH } from "node-ssh";
-
 export type sshCheckResult =
 | {
     status: "success";
     hostname: string;
     username: string;
     password: string;
-    result: NodeSSH;
 } | {
     status: "error";
     message: string;
