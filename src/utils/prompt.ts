e7cc241e9675e8cfd5cdd2e7e1adb4bac3079873
歯臭い <tsurumaki0397@icloud.com>
2025-03-17 18:07:49 +0900

    Update prompt.ts

--- src/utils/prompt.ts
+++ src/utils/prompt.ts
@@ -1,7 +1,9 @@
 export async function prompt(q: string): Promise<string> {
     return new Promise((resolve) => {
         process.stdout.write(`${q}> `);
+        process.stdin.resume();
         process.stdin.once("data", (data) => {
+            process.stdin.pause();
             resolve(data.toString().trim());
         });
     });
